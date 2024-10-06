87a78b6f77655c154afa3db3d524d2f058ae4c9c
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-10-07 01:24:47 +0700

    Connect MongoDB Online

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+require('dotenv').config()
+const database = require('./config/database');
+
+
+
 const app = express()
 const port = 3000;
 
 
+
+// Conect DB
+database.connect();
 
 
 app.get('/',(req, res) => {
