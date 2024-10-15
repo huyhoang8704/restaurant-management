1542e39f1ba088ec252cd421a46ceb8e67075625
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-10-16 05:53:18 +0700

    .

--- routes/admin/dish.route.js
+++ routes/admin/dish.route.js
@@ -3,22 +3,8 @@ const controller = require('../../Controllers/admin/dish.controller')
 const router = express.Router()
 
 
-// router.get('/',controller.index)
 // router.patch('/change-status/:status/:id',controller.changeStatus)
 // router.delete('/delete/:id',controller.deleteItem)
-// // create
-// router.get('/create',controller.create)
-// router.post('/create',controller.createPOST)
-
-
-// // create
-// router.get('/create',controller.create)
-// router.post('/create',controller.createPOST)
-// // edit
-// router.get('/edit/:id',controller.edit)
-// router.patch('/edit/:id',controller.editPatch)
-// //detail
-// router.get('/detail/:id',controller.detail)
 
 
 router.get('/detail/:name', controller.getDish),
