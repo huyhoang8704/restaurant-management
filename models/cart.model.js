36967f37e6c8b403bf372ac421a2ad128c8a18f5
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-10-20 04:20:55 +0700

    update Schema Cart

--- models/cart.model.js
+++ models/cart.model.js
@@ -4,23 +4,10 @@ const CartSchema = new mongoose.Schema({
     customer_id: String,
     dishes: [
         {
-            dish: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Dish',
-                required: [true, 'Dish is required']
-            },
-            quantity: {
-                type: Number,
-                required: [true, 'Quantity is required'],
-                min: [1, 'Quantity must be at least 1']
-            }
+            dish_id : String,
+            quantity : Number,
         }
     ],
-    totalAmount: {
-        type: Number,
-        default: 0,
-        min: [0, 'Total amount must be a positive number']
-    },
 },
     {
         timestamps: true
